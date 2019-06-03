888c97ca9d8eca58ab28feca22596a94c0fec5ee
Berta <nosarabs@gmail.com>
2019-06-02 21:25:54 -0600

    Crea socket del n.n en secureUDP

--- nodoN.py
+++ nodoN.py
@@ -1,40 +1,30 @@
 import socket
+from secureUDP import secureUDP
 
 
 class nodoN():
-    ##constructor de la clase nodo 
+    ##constructor de la clase nodo
 	def __init__(self, ip, puerto): #constructor
 		self.ip = ip
 		self.puerto = puerto
 		self.list = []
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		secureUDP.crearSocket(ip,puerto)
 
-		server_address = ip
-		server_port = puerto
-
-		server = (server_address,server_port)
-		sock.bind(server)
-		print("Listening on " + server_address + ":" + str(server_port))
-
-		while True:
-			payload, client_address = sock.recvfrom(1024)
-			print(payload)
-			
     ##Metodo cargar archivo en una lista de listas desde los argumentos
     ## y la primera posicion es el nombre del nodo seguido de sus vecinos
 	def cargarArchivo():
 		archivo=""
-        if len(sys.argv) >= 2:
-            archivo = sys.argv[1]
-        else:
-            print("Este programa necesita un parámetro");
-            exit(0)
-        with open(archivo) as csvarchivo:
-            entrada = csv.reader(csvarchivo)
-            for dato in entrada:
-                print(dato)    
-                self.list.append(dato)
-    
+		if len(sys.argv) >= 2:
+			archivo = sys.argv[1]
+		else:
+			print("Este programa necesita un parámetro");
+			exit(0)
+			with open(archivo) as csvarchivo:
+				entrada = csv.reader(csvarchivo)
+			for dato in entrada:
+				print(dato)
+				self.list.append(dato)
+
     ##metodo que devuelve una lista de los vecinos solicitados
 	def listaVecinos(nodo):
 		vecinos=[]
@@ -45,9 +35,6 @@ class nodoN():
 						vecinos.append(dato)
 		print(vecinos)
 		return vecinos
-
-
-
 
 
 def main():
--- secureUDP.py
+++ secureUDP.py
@@ -5,6 +5,20 @@ class secureUDP():
 	def __init__(self): #constructor
 		pass
 
+	def crearSocket(ip,puerto):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		server_address = ip
+		server_port = puerto
+
+		server = (server_address,server_port)
+		sock.bind(server)
+		print("Listening on " + server_address + ":" + str(server_port))
+
+		while True:
+			payload, client_address = sock.recvfrom(1024)
+			print(payload)
+
 def main():
 	sUDP = secureUDP()
 
