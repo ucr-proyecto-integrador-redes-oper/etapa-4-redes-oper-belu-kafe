0b4175b689c2c3f400beb9c0e9ac4f3e2d3f7306
Lucho Ramírez <luchoestebanra@gmail.com>
2019-06-03 11:42:33 -0600

    Fix bugs

--- nodoN.py
+++ nodoN.py
@@ -1,42 +1,62 @@
 from secureUDP import secureUDP
+import sys
+import csv
 
 class nodoN():
+	ORANGE_PORT = 9999
+	BLUE_PORT = 19999
     ##constructor de la clase nodo
 	def __init__(self, ip, puerto): #constructor
 		self.ip = ip
 		self.puerto = puerto
 		self.list = []
 		self.mapa = {} #mapa que recibe key como string y una tupla de ip y puerto
-		secureUDP().crearSocket(ip,puerto)
+		self.secureUDP = secureUDP(self.ip, self.puerto)
+		self.cargarArchivo()
 
     ##Metodo cargar archivo en una lista de listas desde los argumentos
     ## y la primera posicion es el nombre del nodo seguido de sus vecinos
-	def cargarArchivo():
+	def cargarArchivo(self):
 		archivo=""
 		if len(sys.argv) >= 2:
 			archivo = sys.argv[1]
 		else:
 			print("Este programa necesita un parámetro");
 			exit(0)
-			with open(archivo) as csvarchivo:
-				entrada = csv.reader(csvarchivo)
+		with open(archivo) as csvarchivo:
+			entrada = csv.reader(csvarchivo, delimiter=',')
 			for dato in entrada:
-				print(dato)
-				self.list.append(dato)
+				vecinos = []
+				for vecino in dato:
+					if(vecino != 0):	
+						vecinos.append(vecino)
+				self.list.append(vecinos)
+		#Debugging
+		for r in range(len(self.list)):
+			print(self.list[r])
 
     ##metodo que devuelve una lista de los vecinos solicitados
-	def listaVecinos(nodo):
+	def listaVecinos(self, nodo):
 		vecinos=[]
-		for fila in list:
-			if fila[0] == nodo:
-				for dato in fila:
-					if dato !='' and dato != nodo:
-						vecinos.append(dato)
-						self.mapa[str(dato[0])] = () #se agrega al mapa con la tupla vacía
+		for dato in self.list[nodo-1]:
+			if dato !='' and dato != str(nodo):
+				vecinos.append(dato)
+				self.mapa[str(dato[0])] = () #se agrega al mapa con la tupla vacía
 		print(vecinos)
 		return vecinos
 
-	def esperarAsignacion():
+	def recibirToken(self):
+		while True:
+			msg = secureUDP.receive(ORANGE_PORT)
+			if msg[0] == "I":
+				#tokenInicial(msg)
+			elif msg[0] == "T":
+				#token(msg)
+
+	def tokenInicial(self, tokenInicial):
+		pass
+
+	def token(self, token):
 		pass
 
 	def actualizarEstructuras(key, ip, puerto):
--- secureUDP.py
+++ secureUDP.py
@@ -2,21 +2,17 @@ import socket
 
 class secureUDP():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	def __init__(self): #constructor
-		pass
+	def __init__(self, ip = "", puerto = 0): #constructor
+		self.crearSocket(ip,puerto)
 
 	def crearSocket(self,ip,puerto):
 		server = (ip,puerto)
 		self.sock.bind(server)
-		print("Listening on " + ip + ":" + str(puerto))
-
-		while True:
-			payload, client_address = self.sock.recvfrom(1024)
-			print(payload)
 
 	def send(datos,ip,puerto):
 		serverAddress = (ip, puerto)
 		self.sock.sendto(datos, serverAddress)
 
 	def receive(puerto):
-		pass
+		msg = self.sock.recvfrom(1024)
+		return msg
