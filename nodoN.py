f28070aca4e1d808e34ece5bd4f819e9405c5a0f
Katherine <katherine.angulomendoza@ucr.ac.cr>
2019-06-02 20:46:26 -0600

    metodos leer csv y devolver vecinos dependiendo del nodo listo

--- nodoN.py
+++ nodoN.py
@@ -2,11 +2,11 @@ import socket
 
 
 class nodoN():
-
+    ##constructor de la clase nodo 
 	def __init__(self, ip, puerto): #constructor
 		self.ip = ip
 		self.puerto = puerto
-
+        self.list = []
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 		server_address = ip
@@ -19,6 +19,34 @@ class nodoN():
 		while True:
 			payload, client_address = sock.recvfrom(1024)
 			print(payload)
+			
+    ##Metodo cargar archivo en una lista de listas desde los argumentos
+    ## y la primera posicion es el nombre del nodo seguido de sus vecinos
+    def cargarArchivo():
+        archivo=""
+        if len(sys.argv) >= 2:
+            archivo = sys.argv[1]
+        else:
+            print("Este programa necesita un parámetro");
+            exit(0)
+        with open(archivo) as csvarchivo:
+            entrada = csv.reader(csvarchivo)
+            for dato in entrada:
+                print(dato)    
+                self.list.append(dato)
+    
+    ##metodo que devuelve una lista de los vecinos solicitados           
+    def listaVecinos(nodo):
+        vecinos=[]
+        for fila in list:
+            if fila[0] == nodo:
+                for dato in fila:
+                    if dato != '' and dato != nodo:
+                        vecinos.append(dato)
+        print(vecinos)
+        return vecinos
+                
+    
 
 def main():
 	servidor = nodoN('0.0.0.0',8888)
