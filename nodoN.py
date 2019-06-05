8912276ef0b16ba856b0deb36342d7a6aa25d134
felipeSolisJimenez <felipesolisjimenez@hotmail.com>
2019-06-04 18:53:44 -0600

    listo mapa,  corregidos bugs

--- nodoN.py
+++ nodoN.py
@@ -2,23 +2,24 @@ from secureUDP import secureUDP
 import sys
 import csv
 
+
 class nodoN():
 	ORANGE_PORT = 9999
 	BLUE_PORT = 19999
-    ##constructor de la clase nodo
-	def __init__(self, ip, puerto): #constructor
+    # constructor de la clase nodo
+	def __init__(self, ip, puerto):  # constructor
 		self.ip = ip
 		self.puerto = puerto
 		self.list = []
 		self.cola = []
-		self.mapa = {} #mapa que recibe key como string y una tupla de ip y puerto
+		self.mapa = {}  # mapa que recibe key como string y una tupla de ip y puerto
 		self.secureUDP = secureUDP(self.ip, self.puerto)
 		self.cargarArchivo()
 
-    ##Metodo cargar archivo en una lista de listas desde los argumentos
-    ## y la primera posicion es el nombre del nodo seguido de sus vecinos
+    # Metodo cargar archivo en una lista de listas desde los argumentos
+    # y la primera posicion es el nombre del nodo seguido de sus vecinos
 	def cargarArchivo(self):
-		archivo=""
+		archivo = ""
 		if len(sys.argv) >= 2:
 			archivo = sys.argv[1]
 		else:
@@ -27,38 +28,41 @@ class nodoN():
 		with open(archivo) as csvarchivo:
 			entrada = csv.reader(csvarchivo, delimiter=',')
 			for dato in entrada:
+				self.mapa[str(dato[0])] = () #Se agregan nodos al mapa, con tupla vacía
 				vecinos = []
 				for vecino in dato:
-					if(vecino != 0):	
+					if(vecino != 0):
 						vecinos.append(vecino)
 				self.list.append(vecinos)
-		#Debugging
-		for r in range(len(self.list)):
-			print(self.list[r])
+		# Debugging
+		# for r in range(len(self.list)):
+		# 	print(self.list[r])
+		print(self.mapa)
 
-    ##metodo que devuelve una lista de los vecinos solicitados
+    # metodo que devuelve una lista de los vecinos solicitados
 	def listaVecinos(self, nodo):
-		vecinos=[]
-		indice=0
-		for reg in list:
-            if reg[0] == nodo:
-                break
-            indice += 1
+		vecinos = []
+		indice = 0
+		for reg in self.list:
+			if reg[0] == nodo:
+				break
+			indice += 1
             
 		for dato in self.list[indice]:
 			if dato !='' and dato != str(nodo):
 				vecinos.append(dato)
-				self.mapa[str(dato[0])] = () #se agrega al mapa con la tupla vacía
-		print(vecinos)
+		#print(vecinos)
 		return vecinos
 
 	def recibirToken(self):
 		while True:
 			msg = secureUDP.receive(ORANGE_PORT)
 			if msg[0] == "I":
-				#self.tokenInicial(msg)
+				# self.tokenInicial(msg)
+				pass
 			elif msg[0] == "T":
-				#self.token(msg)
+				# self.token(msg)
+				pass
 
 	def tokenInicial(self, tokenInicial):
 		pass
@@ -71,10 +75,17 @@ class nodoN():
 			msg = secureUDP.receive(BLUE_PORT)
 			cola.append(msg)
 
-	def actualizarEstructuras(key, ip, puerto):
+	def actualizarEstructuras(self, key, ip, puerto):
 		self.mapa[key] = (ip, puerto)
+		# print(self.mapa)
+		vecinosNodo = self.listaVecinos(key)
+		# print (vecinosNodo)
+		#falta empaquetar esta información y pasarla a azules
+
+		
 
 def main():
 	servidor = nodoN('0.0.0.0',8888)
-
+	#servidor.actualizarEstructuras("9", "1.1.1.1", "5555")
+	
 if __name__ == '__main__': main()
