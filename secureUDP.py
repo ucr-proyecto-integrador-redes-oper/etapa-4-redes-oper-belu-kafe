1a226e8821580e0a0101bdee1a5065eea98ea822
Katherine <katherineam97@gmail.com>
2019-06-16 21:02:57 -0600

    agregados locks y timer a secure udp

--- secureUDP.py
+++ secureUDP.py
@@ -1,4 +1,6 @@
 import socket
+from threading import Lock, Thread
+from time import sleep
 
 class secureUDP():
 	
@@ -6,6 +8,8 @@ class secureUDP():
 		self.SN = 0;
 		self.recibidos = []
 		self.enviados = []
+		self.lockRecv = Lock()
+		self.lockSend = Lock()
 		self.mensajesaProcesar = []
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sock.settimeout(1)
@@ -24,17 +28,22 @@ class secureUDP():
 		self.sock.sendto(datos, serverAddress)
 		parAM = (serverAddress, msgFinal)
 		#tomo lock
+		lockSend.acquire()
 		enviados.append(parAM)
+		lockSend.release()
 		#libero lock
 
 	##Hilo que tiene un timer y cada cierto tiempo revisa la lista
 	def checktoResend(self):
 		while True:
 			#timer
+			sleep(0.03) #cada 30 milisegundos revisa la lista
 			#tomo lock
+			lockSend.acquire()
 			for address, msg in enviados:
 				self.sock.sendto(msg, address)
 			#libero lock
+			lockSend.release()
 			#restart timer
 
 		
@@ -44,7 +53,9 @@ class secureUDP():
 			msg, address = self.sock.recvfrom(1024) #debemos ver tamaño maximo que enviamos
 			parAM = (address, msg)
 			#tomo lock
+			lockRecv.acquire()
 			recibidos.append(parAM)
+			lockRecv.release()
 			#libero lock
 
 	##Hilo que revisa la lista de recibidos y toma acciones necesarias
@@ -59,7 +70,9 @@ class secureUDP():
 						SNE =  int.from_bytes([msj[1], msj[2]], byteorder='big')
 						if SNE == SNR: 
 							#tomo lock
+							lockSend.acquire()
 							del enviados[index]
+							lockSend.release()
 							#libero lock
 						index += 1
 
@@ -67,8 +80,9 @@ class secureUDP():
 					mesage = bytearray([1, msg[1], msg[2]]) #no estoy segura si msg[0] como ya es un byte esto lo acepte 
 					self.sock.sendto(mesage, address) #se envió ack
 					self.mensajesaProcesar.append(msg[3:len(msg)]) #solo el mensaje util que debe ser procesado
+				lockRecv.acquire()
 				del recibidos[recvIndex] #ya tomó la acción necesaria borra el recibido
-
+				lockRecv.release()
 		##Método que usa la capa superior para obtener el mensaje
 		def getMesage(self):
 			if len(mensajesaProcesar) != 0:
