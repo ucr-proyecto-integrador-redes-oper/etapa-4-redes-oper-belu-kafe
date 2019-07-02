d3cd681a45863d29c28ee2b40e14d841e4c68b69
Katherine <katherine.angulomendoza@ucr.ac.cr>
2019-07-02 10:36:50 -0600

    metodos correspondientes a Arbol de expansión minima terminados IDS: 11, 12, 13

--- clientNode.py
+++ clientNode.py
@@ -4,124 +4,163 @@ from struct import *
 from secureUDP import secureUDP
 import random
 from threading import Lock, Thread
+from time import sleep
 import sys # Para pasar argumentos
 import re # Para usar RegEx (Expresiones Regulares)
 
 class ClientNode():
 	
-	def __init__(self, myIp ,serverIP, serverPort):
-		self.localIP = myIp
-		self.localPort = random.randint(10000, 65000)	
-		self.serverIP = serverIP
-		self.serverPort = serverPort
-		self.secureUDP = secureUDP(self.localIP, self.localPort)
-		self.nodoId = 0
-		self.vecinos = []
-		hiloConsola = Thread(target=self.consola, args=())
-		hiloConsola.start()
-		hiloRecvAzul = Thread(target=self.receiveAzul, args=())
-		hiloRecvAzul.start()
-		self.run()
+    def __init__(self, myIp ,serverIP, serverPort):
+        self.JOINTREE = 11
+        self.IDO = 12
+        self.DADDY = 13 
+        self.STARTJOIN = 18
+        self.localIP = myIp
+        self.localPort = random.randint(10000, 65000)	
+        self.serverIP = serverIP
+        self.serverPort = serverPort
+        self.secureUDP = secureUDP(self.localIP, self.localPort)
+        self.nodoId = 0
+        self.vecinos = []
+        self.connected = 0 # variable que dice si estoy conectado al joinTree
+        self.idVecinosArbol = []
+        hiloConsola = Thread(target=self.consola, args=())
+        hiloConsola.start()
+        hiloRecvAzul = Thread(target=self.receiveAzul, args=())
+        hiloRecvAzul.start()
+        self.run()
 	
-	def run(self):
-		self.sendRequest()
-		self.receiveRequest()
+    def run(self):
+        self.sendRequest()
+        self.receiveRequest()
 	
-	def sendRequest(self):
-		msgId = (14).to_bytes(1, byteorder="big")	
-		msgIP = ip_to_bytes(str_ip_to_tuple(self.localIP))
-		msgPort = (self.localPort).to_bytes(2, byteorder="big")
-		msgFinal = (msgId + msgIP + msgPort)
-		print("Sending request...")
-		self.secureUDP.send(msgFinal, self.serverIP, self.serverPort)
-		#Debugging splitting(should be on Orange Node)
+    def sendRequest(self):
+        msgId = (14).to_bytes(1, byteorder="big")	
+        msgIP = ip_to_bytes(str_ip_to_tuple(self.localIP))
+        msgPort = (self.localPort).to_bytes(2, byteorder="big")
+        msgFinal = (msgId + msgIP + msgPort)
+        print("Sending request...")
+        self.secureUDP.send(msgFinal, self.serverIP, self.serverPort)
+        #Debugging splitting(should be on Orange Node)
 
-	def receiveRequest(self):
-		while True:
-			infoNodo = self.secureUDP.getMessage()
-			msgId = int(infoNodo[0])
-			self.nodoId = int.from_bytes(infoNodo[1:3], "big")
-			if int(msgId) == 15:
-				vecino = int.from_bytes(infoNodo[3:5],"big")
-				if self.isRepeated(vecino) == False:
-					self.vecinos.append((vecino, 0, 0))
-			elif int(msgId) == 16:
-				vecino = int.from_bytes(infoNodo[3:5],"big")
-				vecinoIP = ip_tuple_to_str(ip_to_int_tuple(infoNodo[5:9]))
-				vecinoPort = int.from_bytes(infoNodo[9:11],"big")
-				if self.isRepeated(vecino) == False:
-					self.vecinos.append((vecino, vecinoIP, vecinoPort))
-				self.helloVecino(vecinoIP, vecinoPort)
-	
-	def helloVecino(self, vecinoIP, vecinoPort):
-		msgId = (1).to_bytes(1, byteorder="big")
-		nodoId = (self.nodoId).to_bytes(2, byteorder="big") 
-		msgIP = ip_to_bytes(str_ip_to_tuple(self.localIP))
-		msgPort = (self.localPort).to_bytes(2, byteorder="big")
-		msgFinal = (msgId + msgIP + msgPort)
-		print("Enviando " + str(msgFinal))
-		self.secureUDP.send(msgFinal, vecinoIP, self.vecinoPort)
+    def receiveRequest(self):
+        while True:
+            infoNodo = self.secureUDP.getMessage()
+            msgId = int(infoNodo[0])
+            self.nodoId = int.from_bytes(infoNodo[1:3], "big")
+            if int(msgId) == 15:
+                vecino = int.from_bytes(infoNodo[3:5],"big")
+                if self.isRepeated(vecino) == False:
+                    self.vecinos.append((vecino, 0, 0))
+            elif int(msgId) == 16:
+                vecino = int.from_bytes(infoNodo[3:5],"big")
+                vecinoIP = ip_tuple_to_str(ip_to_int_tuple(infoNodo[5:9]))
+                vecinoPort = int.from_bytes(infoNodo[9:11],"big")
+                if self.isRepeated(vecino) == False:
+                    self.vecinos.append((vecino, vecinoIP, vecinoPort))
+                self.helloVecino(vecinoIP, vecinoPort)
+            elif int(msgId) == self.JOINTREE:#si recibo solicitud de unión respondo si estoy en el arbol
+                Ido(int(infoNodo[1:3])) 
+            elif int(msgId) == self.IDO:#si recibo un IDO veo si estoy conectado y si no envío un daddy y agrego a mi papa a la lista idVecinosArbol
+                 if self.connected == 0:
+                    daddy()
+                    self.idVecinosArbol.append(int(infoNodo[1:3]))
+            elif int(msgId) == self.DADDY:#si recibo un daddy agrego el id del nodo a mi lista de idVecinosArbol
+                self.idVecinosArbol.append(int(infoNodo[1:3]))
+            elif int(msgId) == self.STARTJOIN:##Este es el de Berta es un mensaje con solo ese numero que viene de los naranjas a todos los azules que asignó para que comiencen a unirse al grafo, cuando un nodo azul recibe esto pone a correr el hilo joinTree.
+                print("Starting... joinTree")
+                
+    def joinTree(self): ##envía un mensaje a sus vecinos azules para ver si logra conectarse al arbol de expansión minima(DEBE SER UN HILO) 
+        if self.nodoId == 0: #si yo soy el nodo que por defecto ya estoy en el árbol no tengo que intentar unirme
+            self.connected = 1 #pongo mi variable connected como 1
+            return 0
+        while self.connected == 0:
+            msgId = (self.JOINTREE).to_bytes(1, byteorder="big")
+            nodeId = (self.nodoId).to_bytes(2, byteorder="big")
+            msgFinal = (msgId + nodeId)
+            self.secureUDP.send(msgFinal, self.serverIP, self.serverPort)
+        sleep(5)
+            
+    def Ido(self, Idnodo): #se envía mensaje si formo parte del árbol
+        if self.connected == 1:
+            msg = (self.IDO).to_bytes(1, byteorder="big") + (self.nodoId).to_bytes(2, byteorder="big")
+            for elemento in self.vecinos:
+                if elemento[0] == Idnodo :
+                    self.secureUDP.send(msg, elemento[1], elemento[2]) #envia un msj de IDO a otro azul
+    
+    def daddy(self):#Envio un mensaje para avisarle al nodo que escogí para unirme al arbol de expansión minima
+        msg = (self.DADDY).to_bytes(1, byteorder="big") + (self.nodoId).to_bytes(2, byteorder="big")
+        self.connected == 1
+        
+        
+    def helloVecino(self, vecinoIP, vecinoPort):
+        msgId = (1).to_bytes(1, byteorder="big")
+        nodoId = (self.nodoId).to_bytes(2, byteorder="big") 
+        msgIP = ip_to_bytes(str_ip_to_tuple(self.localIP))
+        msgPort = (self.localPort).to_bytes(2, byteorder="big")
+        msgFinal = (msgId + msgIP + msgPort)
+        print("Enviando " + str(msgFinal))
+        self.secureUDP.send(msgFinal, vecinoIP, self.vecinoPort)
 
-	def receiveAzul(self):
-		while True:
-			msg = self.secureUDP.getMessage()
-			if int(msg[0]) == 1:
-				self.vecinoId = int.from_bytes(infoNodo[1:3], "big")
-				vecinoIP = ip_tuple_to_str(ip_to_int_tuple(infoNodo[5:9]))
-				vecinoPort = int.from_bytes(infoNodo[9:11],"big")
-				print("Actualizando vecino " + vecinoId + " con IP " + vecinoIP + " con puerto " + str(vecinoPort))
-				actualizarVecinos(vecinoId, vecinoIP, vecinoPort)
+    def receiveAzul(self):
+        while True:
+            msg = self.secureUDP.getMessage()
+            if int(msg[0]) == 1:
+                self.vecinoId = int.from_bytes(infoNodo[1:3], "big")
+                vecinoIP = ip_tuple_to_str(ip_to_int_tuple(infoNodo[5:9]))
+                vecinoPort = int.from_bytes(infoNodo[9:11],"big")
+                print("Actualizando vecino " + vecinoId + " con IP " + vecinoIP + " con puerto " + str(vecinoPort))
+                actualizarVecinos(vecinoId, vecinoIP, vecinoPort)
 
-	def actualizarVecinos(self, vecinoId, vecinoIP, vecinoPort):
-		for vecino in self.vecinos:
-			if vecino[0] == vecinoId:
-				vecino[1] = vecinoIP
-				vecino[2] = vecinoPort
+    def actualizarVecinos(self, vecinoId, vecinoIP, vecinoPort):
+        for vecino in self.vecinos:
+            if vecino[0] == vecinoId:
+                vecino[1] = vecinoIP
+                vecino[2] = vecinoPort
 
-	def isRepeated(self, nodoId):
-		for n in self.vecinos:
-			if n[0] == nodoId:
-				return True
-		return False
+    def isRepeated(self, nodoId):
+        for n in self.vecinos:
+            if n[0] == nodoId:
+                return True
+        return False
 
-	def consola(self):
-		while True:
-			resp = input()
-			if resp == "1":
-				print("Yo soy " + str(self.nodoId) + " con IP " + self.localIP + " y puerto " + str(self.localPort))
-				for n in self.vecinos:
-					print(n)
+    def consola(self):
+        while True:
+            resp = input()
+            if resp == "1":
+                print("Yo soy " + str(self.nodoId) + " con IP " + self.localIP + " y puerto " + str(self.localPort))
+                for n in self.vecinos:
+                    print(n)
 
 def main():
-	myIp = ""
-	ip = ""
-	port = 0
-	if len(sys.argv) > 2:
-		myHost = str(sys.argv[1])
-		host = str(sys.argv[2])
-		puerto = str(sys.argv[3])
-		regex = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
-		x = re.search(regex, ip)
-		try:
-			if myHost == "localhost" and host == "localhost":
-				ip = host
-				myIp = myHost
-				port = int(puerto)
-			else:
-				ip = host
-				myIp = myHost	
-				port = int(puerto)
-		except:
-			print("Direcciónes IP Invalidas")
-			sys.exit(0)
-	else:
-		print("No ingresó argumentos: ")
-		print("Debe ingresar ip y puerto en los argumentos")
-		sys.exit(0)
+    myIp = ""
+    ip = ""
+    port = 0
+    if len(sys.argv) > 2:
+        myHost = str(sys.argv[1])
+        host = str(sys.argv[2])
+        puerto = str(sys.argv[3])
+        regex = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+        x = re.search(regex, ip)
+        try:
+            if myHost == "localhost" and host == "localhost":
+                ip = host
+                myIp = myHost
+                port = int(puerto)
+            else:
+                ip = host
+                myIp = myHost	
+                port = int(puerto)
+        except:
+            print("Direcciónes IP Invalidas")
+            sys.exit(0)
+    else:
+        print("No ingresó argumentos: ")
+        print("Debe ingresar ip y puerto en los argumentos")
+        sys.exit(0)
 
-	client = ClientNode(myIp, ip, port)
-	
+    client = ClientNode(myIp, ip, port)
+
 if __name__ == '__main__':
-	main()
-		
-		
+    main()
+
